b7b4d6480e7e1baad72fd2c8a5e115763e04f98f
akorede <emiolasaheed1@gmail.com>
2024-05-28 18:12:15 +0100

    Style hero section for desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import img from '../assets/women/women-hero.png';
 const Hero = () => {
   return (
     <div>
-      <div className="sm:hidden h-[60vh] bg-[url('assets/women/women-hero.png')] bg-no-repeat bg-cover bg-center relative">
+      <div className="sm:hidden h-[80vh] bg-[url('assets/women/women-hero.png')] bg-no-repeat bg-cover bg-center w-full relative">
         <div className='backdrop-filter bg-[rgba(255,255,255,0.5)] px-10 rounded-md absolute bottom-[100px] left-[50%] translate-x-[-50%] w-[90%] mx-auto'>
           <h1 className='text-3xl text-center text-neutral-black-5 capitalize px-3 py-2'>
             new collection
@@ -13,8 +13,21 @@ const Hero = () => {
           <button className='px-[2rem] py-3 border rounded-lg w-full'>men</button>
         </div>
       </div>
-      <div >
-
+      <div className="hidden sm:block h-[80vh] bg-[url('assets/women/women-hero-desktop.png')] bg-no-repeat bg-cover bg-center relative w-full">
+        <div className='absolute top-[20%] left-[50px] lg:left-[94px]'>
+          <h1 className='text-[3.5rem] capitalize'>
+            new <br />collection
+          </h1>
+          <p className='py-2 capitalize'>Business Management Consulting Boutique <br /> for Lifestyle Brands</p>
+          <div className='flex items-center gap-3'>
+            <a href="#" className=' border-shade-4 border px-4 py-2 text-shade-4 uppercase rounded-md text-sm font-[600]'>learn more</a>
+            <a href="#" className=' border-shade-4 border px-4 py-2 rounded-md uppercase bg-shade-5 text-white text-sm font-[600]'>shop more</a>
+          </div>
+        </div>
+        <div className='bg-white self-center flex justify-between gap-4 p-3 rounded-md absolute bottom-0 right-[9%]'>
+          <button className='px-[3rem] py-3 border rounded-lg w-full focus:border-shade-5 focus:bg-neutral-gray-4 uppercase focus:text-shade-5 text-sm font-bold font-plus-jakarta'>women</button>
+          <button className='px-[3rem] py-3 border rounded-lg w-full focus:border-shade-5 focus:bg-neutral-gray-4 uppercase focus:text-shade-5 text-sm font-bold font-plus-jakarta'>men</button>
+        </div>
       </div>
     </div>
     
@@ -23,3 +36,5 @@ const Hero = () => {
 
 
 export default Hero
+
+
