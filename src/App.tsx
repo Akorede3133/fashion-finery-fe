03bed37615c096f82a4a931929d23eda560b88ff
akorede <emiolasaheed1@gmail.com>
2024-05-28 06:27:33 +0100

    Set up route for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import AppLayout from "./components/AppLayout"
+import Home from "./pages/Home"
+
 const App = () => {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route element={<AppLayout />}>
+      <Route element={<Home />} path="/"/>
+    </Route>
+  ))
   return (
-    <div>App</div>
+   <RouterProvider router={router} />
   )
 }
 
