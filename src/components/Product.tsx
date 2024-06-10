3d83b64b50a00713dc0bdd034ae5a52a82b1914b
akorede <emiolasaheed1@gmail.com>
2024-06-10 21:39:47 +0100

    Style the decsktop version of the product section

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,24 +8,23 @@ const Product = () => {
   return (
     <div className='w-[90%] mx-auto mt-10'>
       <h3 className=' text-center text-2xl font-plus-jakarta py-4'>Product</h3>
-      <div className='flex relative my-10'>
+      <div className='flex sm:justify-center sm:gap-6 relative my-10'>
         <div>
           <img src={womenProduct1} alt="women product" className=' object-cover rounded-md' />
         </div>
-        <div className='ml-[-55px] mr-[-55px] z-20 mt-[-40px] mb-[-40px] '>
+        <div className='ml-[-55px] sm:ml-0 mr-[-55px] sm:mr-0 z-20 mt-[-40px] sm:mt- mb-[-40px] sm:mt-[-45px] sm:mb-[-88px] sm:mb- '>
           <img src={womenProduct2} alt="women product" className=' object-cover rounded-md' />
         </div>
         <div>
           <img src={womenProduct3} alt="women product" className=' object-cover rounded-md' />
         </div>
       </div>
-      <div className='flex justify-center'>
+      <div className='flex justify-center sm:py-20'>
         <button className=" flex items-center gap-2 bg-tint-5 text-primary-dark-blue capitalize font-semibold px-8 py-2 rounded-md">
           <span>view more</span>
           <HiOutlineChevronRight className=" text-shade-5" />
         </button>
-      </div>
-    
+      </div>   
     </div>
   )
 }
