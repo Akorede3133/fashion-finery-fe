c893e040a3a34383959fd62594a81217aac5c29c
akorede <emiolasaheed1@gmail.com>
2024-08-05 14:54:11 +0100

    Solve build error

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./feature/auth/authSlice.js";
+import authReducer from "./feature/auth/authSlice.ts";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
